be85598ded7b1339a695c8f2b020add3a8d0e0e4
Matthew <matthewchun93@gmail.com>
2021-03-03 11:53:43 -0800

    first

---
+++ client2/src/components/Welcome.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    
+    <div style={{height: "100vh"}} className="d-flex flex-column justify-content-center align-items-center">
+     <h1 className="text-white">Welcome</h1>
+     <br/>
+     
+
+     <h5 className="text-info">This is an unprotected page.  Any user should be able to see this page, regardless of their login status.</h5>
+    </div>
+   
+  )
+};
+
+export default Welcome;

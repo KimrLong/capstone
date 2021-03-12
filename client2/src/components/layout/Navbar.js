ba0623ae4b634d9483c27fe886d459ad5c6380e1
Kim Long <kimrlong09@gmail.com>
2021-03-12 11:03:29 -0500

    stuck on main. took out old forum route

--- client2/src/components/layout/Navbar.js
+++ client2/src/components/layout/Navbar.js
@@ -8,7 +8,7 @@ import  * as GiIcons from 'react-icons/gi'
 import {Link} from 'react-router-dom'
 import {SidebarData} from '../SidebarData'
 import '../../assets/Navbar.css'
-import {IconContext} from 'react-icons'
+import {IconContext} from 'react-icons';
 import {Nav, NavDropdown} from 'react-bootstrap';
 
 // function Navbar (){
@@ -48,40 +48,40 @@ const Navbar =()=> {
         </Nav.Item>
     </Nav>
     );
-        <>
-        <IconContext.Provider value={{color:'#fff'}}>
-            <div className="navbar">
-                <Link to="#" className='menu-bars'> 
-                    <FaIcons.FaBars onClick={showSidebar}/>
-                </Link>
-                <div className="navText">
-                    <Link className="nav-text-right" style={{color:"#fff",paddingRight:"4px"}} to="/signin">Sign in     </Link> or   
-                    <Link style={{color:"#fff",paddingLeft:"4px"}} to="/form">    Sign up today</Link>
-                </div>
-            </div>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className='nav-menu-items' onClick={showSidebar}> 
-                    <li className="navbar-toggle">
-                        <Link to="#" className='menu-bars'>
-                            <AiIcons.AiOutlineClose />
-                        </Link>
-                    </li>
-                    {SidebarData.map((item, index) =>{
-                        return (
-                            <li key={index} className={item.class}>
-                            <Link to={item.path}>
-                                {item.icon}
-                                <span>{item.title}</span>
-                            </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
-            </IconContext.Provider>
-        </>   
-    )
-}
+        // <>
+        // <IconContext.Provider value={{color:'#fff'}}>
+        //     <div className="navbar">
+        //         <Link to="#" className='menu-bars'> 
+        //             <FaIcons.FaBars onClick={showSidebar}/>
+        //         </Link>
+        //         <div className="navText">
+        //             <Link className="nav-text-right" style={{color:"#fff",paddingRight:"4px"}} to="/signin">Sign in     </Link> or   
+        //             <Link style={{color:"#fff",paddingLeft:"4px"}} to="/form">    Sign up today</Link>
+        //         </div>
+        //     </div>
+        //     <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+        //         <ul className='nav-menu-items' onClick={showSidebar}> 
+        //             <li className="navbar-toggle">
+        //                 <Link to="#" className='menu-bars'>
+        //                     <AiIcons.AiOutlineClose />
+        //                 </Link>
+        //             </li>
+        //             {SidebarData.map((item, index) =>{
+        //                 return (
+        //                     <li key={index} className={item.class}>
+        //                     <Link to={item.path}>
+        //                         {item.icon}
+        //                         <span>{item.title}</span>
+        //                     </Link>
+        //                     </li>
+        //                 )
+        //             })}
+        //         </ul>
+        //     </nav>
+        //     </IconContext.Provider>
+        // </>   
+//     )
+// }
 
 
         // <>
@@ -121,6 +121,6 @@ const Navbar =()=> {
         //     </Container>
 //         // </>   
 //     )
-// }
+}
 
 export default Navbar
